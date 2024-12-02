3fc5895365c71155e7f38947530ec152255a377d
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2024-12-02 09:46:48 -0400

    criei o artquivo index e tambem a classe ContaBanco.php, ja fiz os atributos e os metodos e agora vou para os metodos especiais

---
+++ Aula 05b/ContaBanco.php
@@ -0,0 +1,41 @@
+<?php 
+    class ContaBanco {
+        // Atributos 
+        /* 
+        + numConta
+        # tipo
+        - dono
+        - saldo
+        -status
+        */
+        public $numConta;
+        protected $tipo;
+        private $dono;
+        private $saldo;
+        private $status;
+
+        // Métodos
+        /* 
+        + abrirConta()
+        + fecharConta()
+        + depositar()
+        + sacar()
+        + pagarMensal()
+        */
+        public function abrirConta() {
+
+        }
+        public function fecharConta() {
+
+        }
+        public function depositar() {
+
+        }
+        public function sacar() {
+
+        }
+        public function pagarMensal() {
+            
+        }
+    }
+?>
